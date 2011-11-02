5512898a06d22345e6d5020dc9fa891e076fa7d1
Pedro Mass <drom296@gmail.com>
2011-11-01 23:12:57 -0400

    updated the links to match the new file structure

--- page_parts/nav/global_nav.php
+++ page_parts/nav/global_nav.php
@@ -1,7 +1,7 @@
 <div id='global_nav'>
 					<ul>
-						<li><a href='unix.php?page=page_parts/home.php'
-						onclick="addMultiple('page_parts/home.php','page_parts/side_bar_home.php');">home</a></li>
+						<li><a href='unix.php?page=page_parts/home/home.php'
+						onclick="addMultiple('page_parts/home/home.php','page_parts/side_bar_home.php');">home</a></li>
 						<li>|</li>
 						<li><a href='#'>my account</a></li>
 						<li>|</li>
--- page_parts/nav/local_nav.php
+++ page_parts/nav/local_nav.php
@@ -1,7 +1,7 @@
 <div id='local_nav'>
 						<ul>
 							<li><a href='unix.php?page=page_parts/check_mate.php' onclick="addMultiple('page_parts/check_mate.php','page_parts/side_bar_index.php');">index</a></li> 
-							<li><a href='unix.php?page=page_parts/quizzes/file_manip_quiz.php' onclick="loadTermMult('page_parts/quizzes/file_manip_quiz.php','page_parts/side_bar_quizzes.php');">quizzes</a></li>
-							<li class='active'><a href='unix.php?page=page_parts/file_manip_copy.php' onclick="addMultiple('page_parts/file_manip_copy.php','page_parts/side_bar_tutorials.php');">tutorials</a></li>
+							<li><a href='unix.php?page=page_parts/quizzes/file_manip_quiz.php' onclick="loadTermMult('page_parts/quizzes/beginner/file_manip_quiz.php','page_parts/side_bar/side_bar_quizzes.php');">quizzes</a></li>
+							<li class='active'><a href='unix.php?page=page_parts/file_manip_copy.php' onclick="addMultiple('page_parts/beginner/file_manip_copy.php','page_parts/tutorials/side_bar_tutorials.php');">tutorials</a></li>
 						</ul> 
 					</div>
