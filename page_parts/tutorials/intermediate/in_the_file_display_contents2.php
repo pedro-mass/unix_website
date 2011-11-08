fc9cb3cc4e9730bf5998d556385e345dbbc6a5b1
Pedro Mass <drom296@gmail.com>
2011-11-07 22:31:35 -0500

    Completed some of the "in_the_file" section

---
+++ page_parts/tutorials/intermediate/in_the_file_display_contents2.php
@@ -0,0 +1,22 @@
+
+<h2>In the file: Displaying Contents of File</h2>
+<h3>less</h3>
+<p>
+	This command writes out the content of the file, one page at a time. To go to the next page, tap the space bar. To exit out of viewing the contents, hit the q key.
+	<br /><br />
+	Ex: less phone_numbers.txt
+	<br /><br />
+	Another useful command to know is tail. 
+</p>
+
+<h3>tail</h3>
+<p>
+	This command shows the last 10 lines of a file by default. This can be useful if you want to look at the end of a log file to see what went wrong.	
+</p>
+
+<div id="pageNav">
+	<a href='unix.php?page=page_parts/tutorials/intermediate/in_the_file_display_contents.php' onclick="swapMainTerm('page_parts/tutorials/intermediate/in_the_file_display_contents.php');">Previous</a> 
+	| 
+	<a href='unix.php?page=page_parts/tutorials/tutorial_template.php' onclick="swapMainTerm('page_parts/tutorials/tutorial_template.php');">Next</a>
+
+</div>
