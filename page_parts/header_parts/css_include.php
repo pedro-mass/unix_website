2840f5009942a7f568d3051319e0263d28746e3e
Pedro Juan <drom296@gmail.com>
2011-11-24 10:56:49 -0500

    Updated the CSS and JS files to make use of arrays. Makes it easier to maintain.

--- page_parts/header_parts/css_include.php
+++ page_parts/header_parts/css_include.php
@@ -1,8 +1,22 @@
-		<link type='text/css' rel='stylesheet' href='css/index.css' />	
-		<link type='text/css' rel='stylesheet' href='css/content.css' />
-		<link type='text/css' rel='stylesheet' href='css/sidebar.css' />
-		<link type='text/css' rel='stylesheet' href='css/terminal.css' />
-		<link type='text/css' rel='stylesheet' href='css/nav_global.css' />
-		<link type='text/css' rel='stylesheet' href='css/nav_local.css' />
-		<link type='text/css' rel='stylesheet' href='css/accordian.css' />
-		<link type='text/css' rel='stylesheet' href='css/tutorials.css' />
+		<?php
+			$base_url = "/409_unix_site/";
+			
+			// TODO
+			// make the list of css pages into an array
+			$css_pages = new ArrayObject();
+
+			$css_pages[0] = 'css/index.css';
+			$css_pages[1] = 'css/content.css';
+			$css_pages[2] = 'css/sidebar.css';
+			$css_pages[3] = 'css/terminal.css';
+			$css_pages[4] = 'css/nav_global.css';
+			$css_pages[5] = 'css/nav_local.css';
+			$css_pages[6] = 'css/accordian.css';
+			$css_pages[7] = 'css/tutorials.css';
+			
+			// loop over the css pages
+			foreach ($css_pages as $css){
+				// echo out the css link
+				echo "<link type='text/css' rel='stylesheet' href='".$base_url.$css."' />";
+			}
+		?>
--- page_parts/header_parts/javascript_include.php
+++ page_parts/header_parts/javascript_include.php
@@ -1,23 +1,34 @@
-<script type='text/javascript' src="js/tab_manip.js"></script>
-		<script type='text/javascript' src="js/expand_li.js"></script>
-		<script type='text/javascript' src="js/strip_ahref.js"></script>
-		<script type='text/javascript' src="js/ajax.js"></script>
-		<script type='text/javascript' src="js/page_setup.js"></script>
-		<script type='text/javascript' src="js/content.js"></script>
-		<script type='text/javascript' src="js/flip_terminal.js"></script>
-		<script type='text/javascript' src="js/tutorials.js"></script>
-		<script type="text/javascript" src="js/accountreg.js"></script>
-		
-		
-		
-		
-		<!-- terminal js files -->
-		<script src="js/terminal/jsuix_gui.js" type="text/javascript"></script>
-		<script src="js/terminal/jsuix_krnl.js" type="text/javascript"></script>
-		<script src="js/terminal/jsuix_shell.js" type="text/javascript"></script>
-		<script src="js/terminal/jsuix_cmd.js" type="text/javascript"></script>
-		<script src="js/terminal/jsuix_vi.js" type="text/javascript"></script>
-		<script src="js/terminal/jsuix_inv.js" type="text/javascript"></script>
-		<script src="js/terminal/jsuix_man.js" type="text/javascript"></script>
-		<script src="js/terminal/jsuix_rc.js" type="text/javascript"></script>
-		<script src="js/terminal/jsuix_trycatch.js" type="text/javascript"></script>
+<?php
+	$base_url = "/409_unix_site/";
+	
+	// TODO
+	// make the list of js pages into an array
+	$js_pages = new ArrayObject();
+
+	$js_pages[0] = 'js/tab_manip.js';
+	$js_pages[1] = 'js/expand_li.js';
+	$js_pages[2] = 'js/strip_ahref.js';
+	$js_pages[3] = 'js/ajax.js';
+	$js_pages[4] = 'js/page_setup.js';
+	$js_pages[5] = 'js/content.js';
+	$js_pages[6] = 'js/flip_terminal.js';
+	$js_pages[7] = 'js/tutorials.js';
+	$js_pages[8] = 'js/accountreg.js';
+	
+	// // terminal js files
+	$js_pages[9] = 'js/terminal/jsuix_gui.js';
+	$js_pages[10] = 'js/terminal/jsuix_krnl.js';
+	$js_pages[11] = 'js/terminal/jsuix_shell.js';
+	$js_pages[12] = 'js/terminal/jsuix_cmd.js';
+	$js_pages[13] = 'js/terminal/jsuix_vi.js';
+	$js_pages[14] = 'js/terminal/jsuix_inv.js';
+	$js_pages[15] = 'js/terminal/jsuix_man.js';
+	$js_pages[16] = 'js/terminal/jsuix_rc.js';
+	$js_pages[17] = 'js/terminal/jsuix_trycatch.js';
+	
+	// loop over the js pages
+	foreach ($js_pages as $js){
+		// echo out the js link
+		echo "<script type='text/javascript' src='".$base_url.$js."'></script>";
+	}
+?>
