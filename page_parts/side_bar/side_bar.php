55e4c768dcf2b81627a29b4049d8a356699f40b7
Pedro Juan <drom296@gmail.com>
2011-11-27 20:52:38 -0500

    Now mouseOvers control when to hide/show the side bar.

--- page_parts/side_bar/side_bar.php
+++ page_parts/side_bar/side_bar.php
@@ -1,11 +1,11 @@
-<div id='sidebar'>
+<div id='sidebar' onmouseout="hideTab();" onmouseover="showTab();">
 					
 					<div id='sidebar_tab'>
-						<a href='#' onclick='showTab()' id='showtabbutton'>&#171;</a>
-						<a href='#' onclick='hideTab()' id='hidetabbutton'>&#187;</a>
+						<a href='#' id='showtabbutton'>&#171;</a>
+						<a href='#' id='hidetabbutton'>&#187;</a>
 					</div>
 					
-					<div id="sidebar_content">
+					<div id="sidebar_content" >
 						<?php include('side_bar_home.php'); ?>
 					</div>
 				</div>
