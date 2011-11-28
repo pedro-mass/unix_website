074b72d76d0c5a0a3d0c4892e09742d39aa6c444
Pedro Juan <drom296@gmail.com>
2011-11-27 21:16:15 -0500

    Removed code for the beginning and end of the file, except the content.
    Included the removed parts via PHP includes.

--- welcome.php
+++ welcome.php
@@ -1,30 +1,7 @@
 <?php 
-	// include_once('page_parts/header_parts/doctype.php');
-	include('/page_parts/header_parts/doctype.php'); 
+	// Bring in the beginning section template
+	include('/page_parts/header_parts/beginning.php'); 
 ?>
-<html xmlns="http://www.w3.org/1999/xhtml">
-	<head>	
-		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<title>409 | Group Project | team SU</title>
-		<!-- Bring in the CSS links -->
-		<?php include_once('/page_parts/header_parts/css_include.php'); ?>
-		<!-- Bring in the javascript links -->
-		<?php include_once('/page_parts/header_parts/javascript_include.php'); ?>
-	</head>
-	<body onload="page_init()">
-		<div id='wrapper'>
-			<!-- Bring in the header -->
-			<?php include_once('/page_parts/header.php'); ?>
-		
-			<div id='main'><div id='main_wrapper'>		
-				<div id='content'>
-					<!-- Bring in the local nav -->
-					<?php include_once('/page_parts/nav/local_nav.php'); ?>
-
-					<div id='content_target'>						
-						<!-- Bring in the Contetn -->
-						<div id='content_target_inner'>
-
 
 <!--  Content Section -->
 							<?php 
@@ -35,24 +12,7 @@
 								include_once($pageName); 
 							?>
 <!--  Content Section -->							
-							
-							
-							
-							
-							
-						</div>
-
-						<!-- Setup the div for the terminal -->
-						<div id="terminal"></div>
-					</div>					
-				</div>
-				
-				<!-- Bring in the sidebar -->
-				<?php include_once('/page_parts/side_bar/side_bar.php') ?>
-				
-			</div></div>
-
-		</div>
-
-	</body>
-</html>
+<?php 
+	// Bring in the end section template
+	include('/page_parts/header_parts/end.php'); 
+?>
