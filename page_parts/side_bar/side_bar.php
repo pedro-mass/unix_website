0620ab14fce38c371c35c24801151814038eb510
Pedro Mass <drom296@gmail.com>
2011-11-29 18:58:52 -0500

    Merged the conflicts for the side_bar files.

--- page_parts/side_bar/side_bar.php
+++ page_parts/side_bar/side_bar.php
@@ -1,11 +1,11 @@
-<div id='sidebar'>
+<div id='sidebar' onmouseout="hideTab();" onmouseover="showTab();">
 					
-					<div id='sidebar_tab'>
-						<a href='#' onclick='showTab()' id='showtabbutton'>&#171;</a>
-						<a href='#' onclick='hideTab()' id='hidetabbutton'>&#187;</a>
-					</div>
-					
-					<div id="sidebar_content">
-						<?php include('side_bar_home.php'); ?>
-					</div>
-				</div>
+	<div id='sidebar_tab'>
+		<a id='showtabbutton'>&#171;</a>
+		<a id='hidetabbutton'>&#187;</a>
+	</div>
+	
+	<div id="sidebar_content" >
+		<?php include('side_bar_home.php'); ?>
+	</div>
+</div>
--- page_parts/side_bar/side_bar_home.php
+++ page_parts/side_bar/side_bar_home.php
@@ -1,5 +1,5 @@
 						<ul>
 							<li><a href='unix.php?page=page_parts/tutorials/main_tutorials.php&side=tutorial' onclick="swapMultiple('page_parts/tutorials/main_tutorials.php', 'page_parts/side_bar/side_bar_tutorials.php');">Tutorials</a></li>
 							<li><a href='unix.php?page=page_parts/quizzes/main_quizzes.php&side=quiz' onclick="swapMultiple('page_parts/quizzes/main_quizzes.php','page_parts/side_bar/side_bar_quizzes.php');">Quizzes</a></li>
-							<li><a href='unix.php?page=page_parts/index/main_index.php&side=home' onclick="swapMultiple('page_parts/index/main_index.php','page_parts/side_bar/side_bar_home.php');">Index</a>
+							<li><a href='unix.php?page=page_parts/index/main_index.php&side=home' onclick="swapMultiple('page_parts/index/main_index.php','page_parts/side_bar/side_bar_home.php');">Index</a></li>
 						</ul>
